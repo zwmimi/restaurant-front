b0a0b16c67ac89793f56953f80a362eb7fd7e62e
zwmimi <zwmimi1212@gmail.com>
2022-11-08 13:11:37 +0900

    Home.tsxの書き換え

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,15 +5,16 @@ const Home = () => {
     { name: "店舗新規登録", link: "/create" },
     { name: "店舗一覧", link: "/restaurant" },
   ];
+
   return (
     <>
       {topMenus.map((menu) => (
-        <div className="mb-4 mt-4">
+        <div className="mb-4 mt-4" key={menu.link}>
           <div className="py-4">
             <div className="w-11/12 md:max-w-md mx-auto my-4 shadow-x1 border rounded-lg overflow-hidden bg-yellow-100">
               <Link to={menu.link}>
                 <div className="p-4">
-                  <div className="my-4 text-center text-xl text-gray-700 text-semibold">
+                  <div className="my-4 text-center text-xl text-gray-600 text-semibold">
                     {menu.name}
                   </div>
                 </div>
