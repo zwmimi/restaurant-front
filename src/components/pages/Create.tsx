8687959ad9c1903467945b1d543a6f4ba800fbbb
zwmimi <zwmimi1212@gmail.com>
2022-11-08 13:19:34 +0900

    Create.tsxからフラグメント削除

--- src/components/pages/Create.tsx
+++ src/components/pages/Create.tsx
@@ -2,14 +2,12 @@ import { Form } from "../atoms/form/Form";
 
 const Create = () => {
   return (
-    <>
-      <div className="bg-white p-8 rounded-md w-full">
-        <div className=" flex items-center justify-between pb-6">
-          <h2 className="font-semibold">店舗新規登録</h2>
-        </div>
-        <Form />
+    <div className="bg-white p-8 rounded-md w-full">
+      <div className=" flex items-center justify-between pb-6">
+        <h2 className="font-semibold">店舗新規登録</h2>
       </div>
-    </>
+      <Form />
+    </div>
   );
 };
 
