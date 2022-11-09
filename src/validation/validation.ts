61500e5dba81cb15f23adecea5df85d898e8ebfe
zwmimi <zwmimi1212@gmail.com>
2022-11-09 11:07:32 +0900

    urlバリデーション条件変更

--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -5,11 +5,13 @@ export const restaurantFormValidate = (
   restaurant: Restaurant
 ): RestaurantErrorMsg => {
   let errorMessage: RestaurantErrorMsg = { name: [], url: [], description: [] };
+  // 空文字チェック
   restaurant.name === "" && errorMessage.name.push("※入力してください");
   restaurant.url === "" && errorMessage.url.push("※入力してください");
   restaurant.description === "" &&
     errorMessage.description.push("※入力してください");
 
+  // 文字列制限
   restaurant.name.length > 50 &&
     errorMessage.name.push("※50文字以下にしてください");
   restaurant.url.length > 52 &&
@@ -17,7 +19,10 @@ export const restaurantFormValidate = (
   restaurant.description.length > 100 &&
     errorMessage.description.push("※100文字以下にしてください");
 
-  const pattern = /^https?:\/\/tabelog.com\/[\w/:%#\$&\?\(\)~\.=\+\-]+$/;
+  // urlチェック
+  // 例 https://tabelog.com/tokyo/A1234/A123456/1234567/
+  const pattern =
+    /^https?:\/\/tabelog.com\/\w{3,9}\/A\d{4}\/A\d{6}\/\d{7,8}\/$/;
   pattern.test(restaurant.url) ||
     errorMessage.url.push("正しいURLを入力してください");
 
